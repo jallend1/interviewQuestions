d0d2ccf855885329eefae4a85cae61b009605250
jallend1 <jallend1@gmail.com>
2021-01-19 19:02:53 -0800

    Cosmetic code cleanup

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-const QUESTION = document.getElementById('question');
-const CATEGORIES = document.getElementById('categories')
-const GENERATENEW = document.getElementById('generateNew')
+const QUESTION = document.getElementById("question");
+const CATEGORIES = document.getElementById("categories");
+const GENERATENEW = document.getElementById("generateNew");
 
 const activeCategories = [];
 let allQuestions;
 
-async function getQuestions(){
-    try{
-        const data = await fetch('./data/questions.json');
-        const questions = await data.json();
-        allQuestions = questions;
-        generateCategories(questions);
-        pickAQuestion(questions);
-    } catch(error){
-        console.log('Problemo!', error)
-    }
-};
+async function getQuestions() {
+  try {
+    const data = await fetch("./data/questions.json");
+    const questions = await data.json();
+    allQuestions = questions;
+    generateCategories(questions);
+    pickAQuestion(questions);
+  } catch (error) {
+    console.log("Problemo!", error);
+  }
+}
 
 const generateCategories = () => {
-    const categoryList = 
-        Object.keys(allQuestions)
-        .map(category => {
-            activeCategories.push(category);                                                        // All categories active by default, so pops it into the array
-            firstWord = category.split(' ');                                                        // Takes multi-word categories down to just one word for DOM ID/Name
-            // TODO: Returning a string instead of an array! Remove the random commas showing up!
-            return (
-                `<div class="inputs">
+  const categoryList = Object.keys(allQuestions).map((category) => {
+    activeCategories.push(category); // All categories active by default, so pops it into the array
+    firstWord = category.split(" "); // Takes multi-word categories down to just one word for DOM ID/Name
+    // TODO: Returning a string instead of an array! Remove the random commas showing up!
+    return `<div class="inputs">
                     <input checked type="checkbox" id="${firstWord[0]}" name="${firstWord[0]}" />
                     <label for="${firstWord[0]}">
                         ${category}
                     </label>
-                </div>`
-            )
-        });
-        console.log(typeof categoryList)
-        console.dir(categoryList)
-        CATEGORIES.innerHTML = categoryList.join()
-}
+                </div>`;
+  });
+  console.log(typeof categoryList);
+  console.dir(categoryList);
+  CATEGORIES.innerHTML = categoryList.join();
+};
 
 const pickAQuestion = () => {
-    const categoryList = Object.keys(allQuestions);
-    // Selects random category from selected
-    const randomCategoryName = categoryList[Math.floor(Math.random() * activeCategories.length)];
-    // Selects random number within selected category array lengths
-    const randomNumber = Math.floor(Math.random() * allQuestions[randomCategoryName].length);
-    const randomQuestion = allQuestions[randomCategoryName][randomNumber].question;
-    
-    renderQuestion(randomQuestion)
-}
+  const categoryList = Object.keys(allQuestions);
+  // Selects random category from selected
+  const randomCategoryName =
+    categoryList[Math.floor(Math.random() * activeCategories.length)];
+  // Selects random number within selected category array lengths
+  const randomNumber = Math.floor(
+    Math.random() * allQuestions[randomCategoryName].length
+  );
+  const randomQuestion =
+    allQuestions[randomCategoryName][randomNumber].question;
 
-const renderQuestion = question => {
-    QUESTION.innerText = question;
-}
+  renderQuestion(randomQuestion);
+};
 
-const updateCategories = e => {
-    // TODO Switched to starting out with all selected -- Invert this logic to reflect that
-    console.log(activeCategories)
-    pickAQuestion(allQuestions)
-}
+const renderQuestion = (question) => {
+  QUESTION.innerText = question;
+};
 
-CATEGORIES.addEventListener('click', updateCategories);
-GENERATENEW.addEventListener('click', pickAQuestion);
+const updateCategories = (e) => {
+  // TODO Switched to starting out with all selected -- Invert this logic to reflect that
+  console.log(activeCategories);
+  pickAQuestion(allQuestions);
+};
+
+CATEGORIES.addEventListener("click", updateCategories);
+GENERATENEW.addEventListener("click", pickAQuestion);
 
 getQuestions();
